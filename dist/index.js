dbf852811e0835e475331214c42add734c21aebb
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-28 12:21:06 +0000

    Using Third Party package
    used of the inquirer.js package to deal with prompting users and processing responses.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer = require("inquirer");
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
 let todos = [
@@ -9,9 +10,28 @@ let todos = [
     new todoItem_1.TodoItem(4, "Call Joe", true)
 ];
 let collection = new todoCollection_1.TodoCollection("Sandro", todos);
-console.clear();
-console.log(`${collection.userName}'s Todo List (${collection.getItemCounts().incomplete} items to do)`);
-collection.removeComplete();
-collection.getTodoItems(true).forEach((item) => {
-    item.printDetails();
-});
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List (${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach((item) => {
+        item.printDetails();
+    });
+}
+var Commands;
+(function (Commands) {
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then((answers) => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    });
+}
+promptUser();
