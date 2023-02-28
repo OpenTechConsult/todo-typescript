3766f8c9a6b16a3528a2ea795ab553208997f60b
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-28 08:43:24 +0000

    Todo-TypeScript
    Checking the basic data model features
    
    make sure the initial features of the TodoCollection work as expected
    created some todoItem objects and store them in a TodoCollection object.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos = [
+    new TodoItem(1, "Buy flowers"),
+    new TodoItem(2, "Get shoes"),
+    new TodoItem(3, "Collect tickets"),
+    new TodoItem(4, "Call Joe", true)
+];
+
+let collection = new TodoCollection("Sandro", todos);
+
 console.clear();
-console.log("Sandro's Todo List")
+console.log(`${collection.userName}'s Todo List`);
+
+let newId = collection.addTodo("Goo for run");
+let todoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
