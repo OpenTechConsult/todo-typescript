6143b5c39ad8998fbe9f7fac90e861adee9f114e
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-28 08:05:58 +0000

    removing the access keyword public from the beginning of the constructor. Not needed.

--- src/todoItem.ts
+++ src/todoItem.ts
@@ -1,6 +1,6 @@
 export class TodoItem {
 
-    public constructor(public id: number, 
+    constructor(public id: number, 
                         public task: string, 
                         public complete: boolean = false) {
     }
