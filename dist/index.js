e8d4dada47da75fd3c15593591a4d528d25cc71d
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-28 09:03:45 +0000

    Todo-TypeScript
    Adding type information in index.ts to make the purpose of the code easier to read and understand

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+let todos = [
+    new todoItem_1.TodoItem(1, "Buy flowers"),
+    new todoItem_1.TodoItem(2, "Get shoes"),
+    new todoItem_1.TodoItem(3, "Collect tickets"),
+    new todoItem_1.TodoItem(4, "Call Joe", true)
+];
+let collection = new todoCollection_1.TodoCollection("Sandro", todos);
 console.clear();
-console.log("Sandro's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+let newId = collection.addTodo("Goo for run");
+let todoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+// collection.addTodo(todoItem);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 
-let todos = [
+let todos: TodoItem[] = [
     new TodoItem(1, "Buy flowers"),
     new TodoItem(2, "Get shoes"),
     new TodoItem(3, "Collect tickets"),
     new TodoItem(4, "Call Joe", true)
 ];
 
-let collection = new TodoCollection("Sandro", todos);
+let collection: TodoCollection = new TodoCollection("Sandro", todos);
 
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
 
-let newId = collection.addTodo("Goo for run");
-let todoItem = collection.getTodoById(newId);
-console.log(JSON.stringify(todoItem));
+let newId: number = collection.addTodo("Goo for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+// collection.addTodo(todoItem);
